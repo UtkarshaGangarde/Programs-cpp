915c150c91545f46af486ee8c36511a9d1f19de4
UtkarshaGangarde <91488278+UtkarshaGangarde@users.noreply.github.com>
2022-12-05 15:28:54 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"\n enter first number=";
+    cin>>a;
+    cout<<"\n enter second number=";
+    cin>>b;
+    sum=a+b;
+    //cout<<"\n the sum="<<sum;
+    cout<<"sum"<<"="<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
